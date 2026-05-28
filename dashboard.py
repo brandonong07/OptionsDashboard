f96a3499eba11e52cd33c2a508c12b65bb30f786
BrandonOng07 <brandonong07@gmail.com>
2026-05-27 23:52:56 -0700

    separated into two, calls ladder and puts ladder

--- dashboard.py
+++ dashboard.py
@@ -12,9 +12,14 @@ def main():
     
     df = pd.DataFrame(options_data)
     
+    callData = df[df["Direction"] == "CALL"]
+    putData = df[df["Direction"] == "PUT"]
     # Display the options ladder in a table
-    st.subheader("Options Ladder")
-    st.dataframe(df)
+    st.subheader("Calls Ladder")
+    st.dataframe(callData[["Strike", "Underlying", "Bid", "Ask", "IV", "Delta", "Gamma", "Theta", "Scholes", "Vanna", "Charm", "Speed", "Vomma"]])
+    
+    st.subheader("Puts Ladder")
+    st.dataframe(putData[["Strike", "Underlying", "Bid", "Ask", "IV", "Delta", "Gamma", "Theta", "Scholes", "Vanna", "Charm", "Speed", "Vomma"]])
     
     # Create a scatter plot of Strike vs IV colored by Option Type
     st.subheader("Strike vs Implied Volatility")
