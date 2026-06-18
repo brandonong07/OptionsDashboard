96b7a5199994ca103e15ee437eed18e81c797e2d
BrandonOng07 <brandonong07@gmail.com>
2026-06-17 22:23:19 -0400

    starting trend research

--- trend.py
+++ trend.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 
 def main():
     spx = yf.Ticker("^SPX")
-    history = spx.history(period="1d", interval="1m")
-    print(history)
-    ewm20 = history['Close'].ewm(span=20, adjust=False).mean()
-    std_pt = history['Close'].rolling(window=20).std()
-    meanReversionLower = history['Close'] - 1*std_pt
-    meanReversionUpper = history['Close'] + 1*std_pt
+    trend_history = spx.history(period="1y", interval="1d") # data for 1 year, daily candles
+    intra_day_history = spx.history(period="1d", interval="1m")
+    print(intra_day_history)
+    ewm20 = intra_day_history['Close'].ewm(span=20, adjust=False).mean()
+    std_pt = intra_day_history['Close'].rolling(window=20).std()
+    meanReversionLower = intra_day_history['Close'] - 1*std_pt
+    meanReversionUpper = intra_day_history['Close'] + 1*std_pt
     print(meanReversionLower)
     print(meanReversionUpper)
 
@@ -18,7 +19,7 @@ def main():
     plt.title("SPX 1-Minute Close with Mean Reversion Bands")
 
     plt.plot(meanReversionLower, color='red')
-    plt.plot(history['Close'], color= 'black')
+    plt.plot(intra_day_history['Close'], color= 'black')
     plt.plot(meanReversionUpper, color='blue')
 
     plt.show()
